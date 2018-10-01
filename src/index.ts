7ee00dc219ad7add1c1a6904fcf335b9261e5cd9
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-10-01 17:15:32 +0300

    Add example data that is read by the collection and returned as a FeatureStream

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,62 @@ import {Backend, Collection, Link, Query, FeatureStream, Feature} from 'sofp-lib
 
 let SofpExampleBackend = new Backend('SofpExampleBackend');
 
-class MockCollection implements Collection {
-    name : string = 'mock-collection';
-    description : string = 'This is a mock collection packaged from sofp-mock-backend';
-    links : Link[] = [{
-        href:     'https://www.spatineo.com',
-        rel:      'producer',
-        type:     'text/html',
-        hreflang: 'en',
-        title:    'Spatineo Website'
-    }];
+interface GeoJSONGeometry {
+    type : string;
+    coordinates : Number[];
+};
+
+interface GeoJSONFeature {
+    type : string;
+    properties : Map<String, any>;
+    geometry: GeoJSONGeometry;
+};
+
+interface GeoJSONFeatureCollection {
+    type: string;
+    name: string;
+    crs: object;
+    features: GeoJSONFeature[];
+};
+
+class GeoJSONCollection implements Collection {
+    name : string;
+    description : string;
+    links : Link[] = [];
+
+    data : GeoJSONFeatureCollection;
+
+    constructor(name, description, jsonFile) {
+        this.name = name;
+        this.description = description;
+        this.data = require(process.cwd()+'/'+jsonFile);
+    }
 
     executeQuery(query : Query) : FeatureStream {
         var ret = new FeatureStream();
+        // This example does no filtering of it's own
         ret.remainingFilter = query.filters;
-        ret.push(null);
+        var idx = 0;
+        var that = this;
+
+        function next() {
+            if (idx >= that.data.features.length) {
+                ret.push(null);
+                return;
+            }
+            var item = that.data.features[idx];
+            
+            idx++;
+            ret.push(item);
+            setTimeout(next, 5);
+        }
+        
+        setTimeout(next, 5);
+
         return ret;
     }
 };
 
-SofpExampleBackend.collections.push(new MockCollection());
+SofpExampleBackend.collections.push(new GeoJSONCollection('forecast', 'Example forecast data by FMI', 'data/forecast.json'));
 
 export {SofpExampleBackend};
